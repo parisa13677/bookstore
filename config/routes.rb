4a2cfc363606428f863bba020b455571080d2d2f
parisa <parisa.golanbary@gmail.com>
2018-08-27 13:10:55 +0430

    add controller about and books to bookstore

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/create'
+  get 'books/update'
+  get 'books/edit'
+  get 'books/destroy'
+  get 'books/index'
+  get 'books/show'
   get 'about/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
