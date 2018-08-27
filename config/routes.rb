200e4890dce33ae50490117e595a0b72f0df7877
parisa <parisa.golanbary@gmail.com>
2018-08-27 15:09:16 +0430

    add class well to index.html and add the body to application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'publishers/new'
+  get 'publishers/create'
+  get 'publishers/update'
+  get 'publishers/edit'
+  get 'publishers/destroy'
+  get 'publishers/index'
+  get 'publishers/show'
+  get 'authors/new'
+  get 'authors/create'
+  get 'authors/update'
+  get 'authors/edit'
+  get 'authors/destroy'
+  get 'authors/index'
+  get 'authors/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/update'
+  get 'categories/edit'
+  get 'categories/destroy'
+  get 'categories/index'
+  get 'categories/show'
   get 'books/new'
   get 'books/create'
   get 'books/update'
@@ -7,5 +28,9 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'about/index'
+  resources :books
+  resources :authors
+  resources :publishers
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
