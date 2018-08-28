5f43c1b717e6fc03b13bc35eccee7a354e560cd6
parisa <parisa.golanbary@gmail.com>
2018-08-28 13:17:23 +0430

    create models book,author,publisher,category in model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_28_083750) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id"
+    t.integer "author_id"
+    t.integer "publisher_id"
+    t.string "isbn"
+    t.string "year"
+    t.string "price"
+    t.string "buy"
+    t.text "excerpt"
+    t.string "format"
+    t.integer "pages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
