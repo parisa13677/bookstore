65312e679c78e028356e28054becd6632302cabd
parisa <parisa.golanbary@gmail.com>
2018-08-28 15:24:48 +0430

    add class private to controllers:book,author,publisher,.. to execute.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :category
+  belongs_to :author
+  belongs_to :publisher
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_083750) do
+ActiveRecord::Schema.define(version: 2018_08_28_084353) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
