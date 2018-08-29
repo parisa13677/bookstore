ff8fba029fdbc23484d5a4d9570985315434afb9
parisa <parisa.golanbary@gmail.com>
2018-08-29 12:48:31 +0430

    add code index method of book controller & add this to slidebar.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,18 +13,31 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-
+    flash[:notice] = 'Book Added'
     redirect_to books_path
   end
 
-  def update; end
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = 'Book Updated'
+    redirect_to books_path
+  end
 
-  def edit; end
+  def edit
+    @book= Book.find(params[:id])
+  end
 
-  def destroy; end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'Book Removed'
+    redirect_to books_path
+  end
 
   def index
     @books = Book.all
+    @categories = Category.all
   end
 
   def show
@@ -35,6 +48,7 @@ class BooksController < ApplicationController
 private
 
   def book_params
-    params.require(:book).permit(:title, :category_id, :author_id, :publisher_id, :isbn, :price, :buy, :format, :excerpt, :pages, :year, :coverpath)
+    params.require(:book).permit(:title, :category_id,
+      :author_id, :publisher_id, :isbn, :price, :buy, :format, :excerpt, :pages, :year, :coverpath)
   end
 end
